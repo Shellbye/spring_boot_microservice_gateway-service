0ed302eb9266ca51c7a9dc626a62904755c996fd
shellbye <shellbye.com@gmail.com>
2018-12-04 20:19:00 +0800

    EnableZuulProxy

--- src/main/java/com/shellbye/gateway/GatewayApplication.java
+++ src/main/java/com/shellbye/gateway/GatewayApplication.java
@@ -3,9 +3,11 @@ package com.shellbye.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableZuulProxy
 public class GatewayApplication {
 
 	public static void main(String[] args) {
