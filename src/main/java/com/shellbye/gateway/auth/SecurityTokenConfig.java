a338d8cd115a0a4b25377cf032127fc51ff332e4
shellbye <shellbye.com@gmail.com>
2018-12-05 14:51:43 +0800

    add rest-service(msg) and allow /msg/** for all

--- src/main/java/com/shellbye/gateway/auth/SecurityTokenConfig.java
+++ src/main/java/com/shellbye/gateway/auth/SecurityTokenConfig.java
@@ -33,6 +33,8 @@ public class SecurityTokenConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 // allow all who are accessing "auth" service
                 .antMatchers(HttpMethod.POST, jwtConfig.getUri()).permitAll()
+                // allow /msg
+                .antMatchers("/msg/**").permitAll()
                 // must be an admin if trying to access admin area (authentication is also required here)
                 .antMatchers("/" + "/admin/**").hasRole("ADMIN")
                 // Any other request must be authenticated
